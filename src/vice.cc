86b5ce645052a0c8009c3a1e7df0c17f4f855d23
Nave-Neel <nave.mvn@gmail.com>
2015-10-25 05:43:07 -0500

    Adding server side support for readdir

--- src/vice.cc
+++ src/vice.cc
@@ -7,6 +7,9 @@
 #include <time.h>
 #include <fcntl.h>
 
+#include <sys/stat.h>
+#include <dirent.h>
+
 #include <grpc++/grpc++.h>
 
 #include "../proto/AFSInterface.grpc.pb.h"
@@ -23,14 +26,48 @@ using grpc::Status;
 
 using RpcPackage::StringMessage;
 using RpcPackage::StatStruct;
+using RpcPackage::DirMessage;
+using RpcPackage::DirEntry;
 using RpcPackage::RpcService;
+
+std::string* root_dir;
 
 class Vice final: public RpcService::Service{
 	Status stat_get_attr(ServerContext* context, const StringMessage* recv_msg, StatStruct* reply_struct) override {
-		std::string reply = std::string("ACK:" + recv_msg->msg());
+		struct stat stbuf;
+		memset(&stbuf, 0, sizeof(struct stat));
+		string full_path = *root_dir + recv_msg->msg();
+		log(recv_msg->msg());
+		log(full_path);
+		int res = lstat(full_path.c_str(), &stbuf);
 		reply_struct->set_file_mode(S_IFREG | 0444);
 		reply_struct->set_hard_links(1);
 		reply_struct->set_file_size(4096);
+		return Status::OK;
+	}
+
+	Status readdirectory(ServerContext* context, const StringMessage* recv_msg, DirMessage* reply_msg) override {
+		string full_path = *root_dir + recv_msg->msg();
+		DIR *dp;
+		struct dirent *de;
+		dp = opendir(full_path.c_str());
+		if (dp == NULL){
+			reply_msg->set_success(false);
+			return Status::OK;
+		}
+		reply_msg->set_success(true);
+		while ((de = readdir(dp)) != NULL) {
+			struct stat st;
+			memset(&st, 0, sizeof(st));
+			DirEntry dirEntry;
+			StatStruct stat;
+			stat.set_file_number(de->d_ino);
+			stat.set_file_mode(de->d_type << 12);
+			dirEntry.set_allocated_stat(&stat);
+			DirEntry* ent = reply_msg->add_dir();
+			ent->set_name(de->d_name);
+		}
+		closedir(dp);
 		return Status::OK;
 	}
 };
@@ -55,7 +92,8 @@ void run_server() {
 }
 
 int main(int argc, char** argv) {
-	cout<<string(argv[1])<<endl;
+	open_err_log();
+	root_dir = new string(argv[1]);
 	run_server();
 	return 0;
 }
