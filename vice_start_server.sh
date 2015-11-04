41464cc5f5abee96d8cae261d4ec3a5f3d73176a
Nave-Neel <nave.mvn@gmail.com>
2015-11-04 13:09:09 -0600

    Dont reflush root directroy on each server startup

--- vice_start_server.sh
+++ vice_start_server.sh
@@ -1,12 +1,10 @@
 ROOT_FS_PATH=~/afs-server-root
 
 #check if afs_root dir exists
-if [ -d $FS_PATH ]; then
-	rm -rf $ROOT_FS_PATH
+if [ ! -d $FS_PATH ]; then
+	mkdir $ROOT_FS_PATH
+	cp -r server_root_test/* $ROOT_FS_PATH
 fi
-
-mkdir $ROOT_FS_PATH
-cp -r server_root_test/* $ROOT_FS_PATH
 
 make vice
 bin/vice $ROOT_FS_PATH
