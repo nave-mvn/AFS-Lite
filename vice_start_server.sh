e1f28ac1885db662483043dd3feb336e812c74b8
Nave-Neel <nave.mvn@gmail.com>
2015-11-04 22:17:25 -0600

    Fixed spelling

--- vice_start_server.sh
+++ vice_start_server.sh
@@ -1,7 +1,7 @@
 ROOT_FS_PATH=~/afs-server-root
 
 #check if afs_root dir exists
-if [ ! -d $FS_PATH ]; then
+if [ ! -d $ROOT_FS_PATH ]; then
 	mkdir $ROOT_FS_PATH
 	cp -r server_root_test/* $ROOT_FS_PATH
 fi
